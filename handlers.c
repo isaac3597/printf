9924726bd726755028f770a4f7438eb2e8fcfde8
Isaac Chebii <isaacchebii3597@gmail.com>
2023-07-25 11:15:21 +0300

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -19,7 +19,7 @@
  * @size: Size specifier
  *
  * Return: Number of chars printed.
- */
+ **/
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
